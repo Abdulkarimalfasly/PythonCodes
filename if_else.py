1193c631f580d47c96a8ea416a96f5335efbc46d
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-09-08 15:30:13 -0500

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,10 @@
+time_100m = float(input("Enter the number "))
+if time_100m <=12:
+    print("excellent")
+elif time_100m <= 15:
+    print("very good")
+elif time_100m <= 18:
+    print("acceptable")
+else:
+    print("weak")
+    
