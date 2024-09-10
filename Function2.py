7dcdc293e11a63a57cbc31bd2e72aabb7bc24f55
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-09-09 22:22:39 -0500

    Create Function2.py

---
+++ Function2.py
@@ -0,0 +1,12 @@
+def instructions():
+    print("""
+Instructions:
+1. You can move in a straight line.
+2. It's not allowed to move backwards.
+3. Remember: you have three lives only.
+4. Type 'INST' anytime to show the instructions again.
+""")
+
+# Call the function to display the instructions
+instructions()
+instructions()
