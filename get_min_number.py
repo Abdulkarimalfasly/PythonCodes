4f3404d5bd618c8f3bde97a61840614c995705e4
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-09-11 21:25:50 -0500

    get_min_number.py

---
+++ get_min_number.py
@@ -0,0 +1,7 @@
+def smallest_num(numbers):
+    return min(numbers)
+
+numbers = (23, 345, 567, 26, 45, 67, 89, 56, 3456, 5768, 3445, 7876, 6534)
+smallest = smallest_num(numbers)
+print("The smallest number is:", smallest)
+
