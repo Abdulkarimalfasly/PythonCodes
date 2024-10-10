62266c915761aa0fe33ad4c42380dc656b5d6e97
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-10-09 23:13:37 -0500

    Create_set_demo.py

---
+++ set_demo.py
@@ -0,0 +1,18 @@
+x = [1,7,3,4,5,6,7,5,9,5]
+y = set(x)
+print(x)
+print(y)
+y.add(77)
+print(y)
+y.remove(5)
+print(y)
+print("")
+z = {4,5,67,8}
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+n = 20
+name = "Ali"
+print(type(n))
+print(type(name))
