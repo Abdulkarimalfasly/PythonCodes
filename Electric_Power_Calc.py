0b96209f69714eb0e9f5b30611a7d4508ea751ea
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-11-12 19:00:58 -0600

    Create Electric_Power_Calc.py

---
+++ Electric_Power_Calc.py
@@ -0,0 +1,7 @@
+def circuitPower(voltage, current):
+    return voltage * current
+
+# Test examples
+print(circuitPower(230, 10))  # ➞ 2300
+print(circuitPower(110, 3))   # ➞ 330
+print(circuitPower(480, 20))  # ➞ 9600
