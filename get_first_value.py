d631a50937fdf6970f13cfcf3df18132a4f89a6d
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-11-17 13:58:09 -0600

    Create get_first_value.py

---
+++ get_first_value.py
@@ -0,0 +1,6 @@
+def get_first_value(arr):
+    return arr[0]  # Return the first element of the array
+
+# Examples of usage:
+print(get_first_value([80, 5, 100]))   # ➞ 80
+print(get_first_value([-500, 0, 50]))  # ➞ -500
