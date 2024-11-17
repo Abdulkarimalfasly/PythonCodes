4f82f1e49424edc2e26a61503d454519e76678b1
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-11-16 19:50:48 -0600

    Create sum_comparator.py

---
+++ sum_comparator.py
@@ -0,0 +1,7 @@
+def lessThan100(a, b):
+    return (a + b) < 100
+
+# Examples of usage:
+print(lessThan100(22, 15))  # ➞ True
+print(lessThan100(83, 34))  # ➞ False
+print(lessThan100(3, 77))   # ➞ True
