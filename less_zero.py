f446f0600a50ed3d1a7b144d2e23926851bcba94
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-11-28 13:28:55 -0600

    Create less_zero.py

---
+++ less_zero.py
@@ -0,0 +1,11 @@
+def lessThanOrEqualToZero(num):
+    # Check if the number is less than or equal to zero
+    if num <= 0:
+        return True  # Return True if the condition is met
+    else:
+        return False  # Return False if the condition is not met
+
+# Examples:
+print(lessThanOrEqualToZero(5))   # ➞ False
+print(lessThanOrEqualToZero(0))   # ➞ True
+print(lessThanOrEqualToZero(-2))  # ➞ True
