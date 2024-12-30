9748f4bd16cf348994a85c79305938db5e418212
Abdulkarim Alfasly <1029474@rochesterschools.org>
2024-12-29 22:26:15 -0600

    Create return_argument_function.py

---
+++ return_argument_function.py
@@ -0,0 +1,8 @@
+def arrow_func(arg):
+    # Simply return the given argument
+    return arg
+
+# Test cases
+print(arrow_func(3))     # ➞ 3
+print(arrow_func("3"))   # ➞ "3"
+print(arrow_func(True))  # ➞ True
