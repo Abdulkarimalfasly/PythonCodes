9cd8781ce367148696dc923aa1520400f0a3f867
Abdulkarim Alfasly <1029474@rochesterschools.org>
2025-01-01 09:54:42 -0600

    Create return_argument.py

---
+++ return_argument.py
@@ -0,0 +1,8 @@
+def return_argument(arg):
+    # This function takes an argument and returns it as it is
+    return arg
+
+# Test the function with examples:
+print(return_argument(3))    # Should return 3
+print(return_argument("3"))  # Should return "3"
+print(return_argument(True)) # Should return True
